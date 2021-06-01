e1d48614aa42383bc59aebb311329e104c08501f
Shayna27 <61741756+Shayna27@users.noreply.github.com>
2021-06-01 12:00:50 -0500

    Update listado.php

--- codeingniter/app/Views/listado.php
+++ codeingniter/app/Views/listado.php
@@ -11,17 +11,23 @@
     <title>Hello, world!</title>
   </head>
   <body>
-   <div class="jumbotron">
-  <h1 class="display-4">Shayna Camila Romero Cordova</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-  </p>
-</div> 
-    <h1>Hello, world!</h1>
-
+    <div class="container">
+      <h1>Agregar Registro</h1>
+     <div class="row">
+       <div class="col-sm-12">
+         <form action=" ">
+           <label for="nombre">Nombre:</label>
+           <input type="text" id="nombre" name="nombre" class="form-control" required>
+           <label for="apaterno">Apellido Paterno:</label>
+           <input type="text" id="apaterno" name="apaterno" class="form-control" required>
+           <label for="amaterno">Apellido Materno:</label>
+           <input type="text" id="amaterno" name="amaterno" class="form-control" required>
+           <button class="btn btn-primary form-control">Guardar</button>
+         </form>
+       </div>
+      </div>
+    </div>
+    
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
