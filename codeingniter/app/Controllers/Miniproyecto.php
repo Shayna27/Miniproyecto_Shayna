80b5cf9158d410d5af0d5ff4cdb20445a1dea86d
Shayna27 <61741756+Shayna27@users.noreply.github.com>
2021-06-17 12:08:33 -0500

    Update Miniproyecto.php

--- codeingniter/app/Controllers/Miniproyecto.php
+++ codeingniter/app/Controllers/Miniproyecto.php
@@ -2,17 +2,82 @@
 namespace App\Controllers;
 use App\Models\DatosModel;
 
-class Miniproyecto extends BaseController
-{
-	public function index()
-	{
-		$model = new DatosModel();
-		$datos = $model->listar();
-		$data = [
-			
-			"datos" => $datos
-		];
+class General extends BaseController{
+	
+	public function index(){
+		
+		$gModel = new DatosModel();
+		$mensaje = session('mensaje');	
+		$datos = $gModel->listarTodo();
+		$data = ["datos" => $datos,
+			 "mensaje" => $mensaje
+			 
+	        ];
 		return view('listado',$data);
+		
 	}
-}
-?>
+	
+	public function obtenerDatos($id){
+		$gModel = new DatosModel();
+		$data = ["id" => $id];
+		$respuesta = $gModel->obtenerInformacion($data);
+		
+		$datos = ["datos" => $respuesta];
+		return view('actualizar',$datos);
+		
+	}
+	
+	public function insertar()}
+            $gModel = new DatosModel();
+            $data = [
+		"nombre" => $_POST['nombre'],
+		"a_paterno" $_POST['apaterno'],
+		"a_materno" $_POST['amaterno'],    
+	    ];
+            $respuesta = $gModel->insertar($data);
+       
+            if ($respuesta > 0){
+		return redirect()->to(base_url('/index.php'))->with('mensaje','0');
+	    }else{
+		return redirect()->to(base_url('/index.php'))->with('mensaje','1');  
+	    }
+
+       }
+
+
+
+       public function actualizar(){
+	       
+	   $gModel = new DatosModel();
+	   $data = [
+	       "nombre" => $_POST['nombre'],
+	       "a_paterno" $_POST['apaterno'],
+	       "a_materno" $_POST['amaterno'],
+		   
+	   ];
+	       
+	   $id = ["id" => $_POST['id']];
+	   $respuesta = $gModel->actualizar($data,$id);
+	       
+	   if ($respuesta){
+	       return redirect()->to(base_url('/index.php'))->with('mensaje','2');
+	    }else{
+	       return redirect()->to(base_url('/index.php'))->with('mensaje','3');
+	    }
+	    
+       }
+
+       public function eliminar($idPersona){
+	   $gModel = new DatosModel();
+	   $id = ["id" => $idPersona];
+	   $respuesta = $gModel->eliminar($id);
+	       
+	   if ($respuesta){
+	       return redirect()->to(base_url('/index.php'))->with('mensaje','4');
+	    }else{
+	       return redirect()->to(base_url('/index.php'))->with('mensaje','5');
+	    }
+	    
+       }
+}    
+	       
